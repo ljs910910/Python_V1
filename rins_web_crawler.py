c2a7180f4526bbcb8c7e397c1e1140e60b8640fd
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2021-02-26 14:27:40 +0900

    RINS 트래픽 체크 및 API 이력 조회 웹 크롤러
    7일간의 데이터 수집
    일별 최댓값 추출 후 엑셀 저장

--- rins_web_crawler.py
+++ rins_web_crawler.py
@@ -6,7 +6,6 @@ import time
 import openpyxl
 
 load_wb = openpyxl.Workbook()
-
 def create_excel():
     load_ws = load_wb.active
     load_wb.create_sheet(index=1, title='total_sheet')
@@ -23,7 +22,6 @@ def create_date():
         result = a_week_ago.strftime('%Y-%m-%d')
         load_ws2 = load_wb['total_sheet']
         load_ws2[cell.pop()] = result
-
         # 금 ~ 목 API 이력관리 엑셀 파일 다운
         driver.get('http://172.16.42.50/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
         
@@ -35,46 +33,48 @@ while True:
         driver.find_element_by_name('j_username').send_keys('rins_admin')
         driver.find_element_by_name('j_password').send_keys('ipaleldj1!')
         driver.find_element_by_xpath("""//*[@id="loginForm"]/fieldset/p/input""").click()
+
+        for k in range(-7, 0):  # 금 ~ 목 시트 생성 후, 일별 데이터 저장
+            day_delta = datetime.timedelta(days=1)
+            start_date = datetime.date.today()
+            a_week_ago = start_date + k * datetime.timedelta(1)
+            result = a_week_ago.strftime('%Y%m%d')
+            load_wb.create_sheet(index=1, title=result)
+            load_ws = load_wb[result]
+            driver.get('http://172.16.42.50/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            time.sleep(1)
+            for a_tag in soup.findAll('td', class_='ac'):
+                result1 = (a_tag.get_text().strip().replace('\n', '').replace('\t', ''))
+                load_ws = load_wb[result]
+                load_ws.append([result1])
+
+        cell1 = ['G2', 'F2', 'E2', 'D2', 'C2', 'B2', 'A2']
+        for j in range(-7, 0):  # 최댓값 비교
+            day_delta = datetime.timedelta(days=1)
+            start_date = datetime.date.today()
+            a_week_ago = start_date + j * datetime.timedelta(1)
+            result = a_week_ago.strftime('%Y%m%d')
+            load_ws = load_wb[result]
+            if load_ws['A6'].value < load_ws['A9'].value:
+                temp3 = load_ws['A9'].value
+            else:
+                temp3 = load_ws['A6'].value
+            for q in range(9, 76, 3):
+                n3 = load_ws['A' + str(q)].value
+                if temp3 < n3:
+                    temp3 = n3
+                else:
+                    pass
+            load_ws2 = load_wb['total_sheet']
+            load_ws2[cell1.pop()] = temp3
+            print(result, temp3)
+
     except NoSuchElementException as e:
         print('retry', e)
         continue
     break
-
-for k in range(-7, 0):  # 금 ~ 목 시트 생성 후, 일별 데이터 저장
-    day_delta = datetime.timedelta(days=1)
-    start_date = datetime.date.today()
-    a_week_ago = start_date + k * datetime.timedelta(1)
-    result = a_week_ago.strftime('%Y%m%d')
-    load_wb.create_sheet(index=1, title=result)
-    load_ws = load_wb[result]
-    driver.get('http://172.16.42.50/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    time.sleep(1)
-    for a_tag in soup.findAll('td', class_='ac'):
-        result1 = (a_tag.get_text().strip().replace('\n', '').replace('\t', ''))
-        load_ws = load_wb[result]
-        load_ws.append([result1])
-
-cell1 = ['G2', 'F2', 'E2', 'D2', 'C2', 'B2', 'A2']
-for j in range(-7, 0):  # 최댓값 비교
-    day_delta = datetime.timedelta(days=1)
-    start_date = datetime.date.today()
-    a_week_ago = start_date + j * datetime.timedelta(1)
-    result = a_week_ago.strftime('%Y%m%d')
-    load_ws = load_wb[result]
-    if load_ws['A6'].value < load_ws['A9'].value:
-        temp3 = load_ws['A9'].value
-    else:
-        temp3 = load_ws['A6'].value
-    for q in range(9, 76, 3):
-        n3 = load_ws['A' + str(q)].value
-        if temp3 < n3:
-            temp3 = n3
-        else: pass
-    load_ws2 = load_wb['total_sheet']
-    load_ws2[cell1.pop()] = temp3
-    print(result, temp3)
 
 create_date()
 load_wb.remove(load_wb['Sheet'])
