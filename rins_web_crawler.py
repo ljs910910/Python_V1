c5be9824a02a6d551e7eb9ad28e90d5a3f1c2875
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2021-03-04 12:37:43 +0900

    RINS 트래픽 체크 및 API 이력 조회 웹 크롤러
    7일간의 데이터 수집
    일별 최댓값 추출 후 엑셀 저장
    API 이력조회 엑셀 파일 다운

--- rins_web_crawler.py
+++ rins_web_crawler.py
@@ -23,15 +23,15 @@ def create_date():
         load_ws2 = load_wb['total_sheet']
         load_ws2[cell.pop()] = result
         # 금 ~ 목 API 이력관리 엑셀 파일 다운
-        driver.get('http://172.16.42.50/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
+        driver.get('http://172.16.1.1/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
         
 # 상용 cms 로그인
 while True:
     try:
         driver = webdriver.Chrome(r'C:\Users\webiznet\Desktop\기타\chromedriver.exe')
-        driver.get('http://172.16.42.50/spa/account/login.do')
-        driver.find_element_by_name('j_username').send_keys('rins_admin')
-        driver.find_element_by_name('j_password').send_keys('ipaleldj1!')
+        driver.get('http://172.16.1.1/spa/account/login.do')
+        driver.find_element_by_name('j_username').send_keys('admin')
+        driver.find_element_by_name('j_password').send_keys('passwd')
         driver.find_element_by_xpath("""//*[@id="loginForm"]/fieldset/p/input""").click()
 
         for k in range(-7, 0):  # 금 ~ 목 시트 생성 후, 일별 데이터 저장
@@ -41,7 +41,7 @@ while True:
             result = a_week_ago.strftime('%Y%m%d')
             load_wb.create_sheet(index=1, title=result)
             load_ws = load_wb[result]
-            driver.get('http://172.16.42.50/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
+            driver.get('http://172.16.1.1/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
             time.sleep(1)
