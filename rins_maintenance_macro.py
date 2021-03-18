6337ea629110dfe3d25bceeb699302eca5323f13
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2021-03-18 15:06:22 +0900

    RINS 정기 점검 매크로
    자사 특정 프로그램을 통해 서버 접속이 가능하며, 자동화 툴 및 스크립트 사용이 불가능한 환경
    네트워크가 불안정하여 서버 접속이 원활하지 않아, 첫 번째 서버 접속 후 ssh 명령어로 서버 이동
    호스트명을 캡처하여 광학 문자 인식 진행, 해당 서버가 일치할 경우만 점검 진행

--- rins_maintenance_macro.py
+++ rins_maintenance_macro.py
@@ -8,10 +8,6 @@ import datetime
 from PIL import Image, ImageGrab
 import pytesseract
 import re
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-import openpyxl
 import cv2
 
 warnings.simplefilter('ignore', category=UserWarning)
@@ -168,6 +164,16 @@ while cnt < 1:
         time.sleep(10)
         pyautogui.hotkey('ctrl', 'c')
         time.sleep(1)
+        pyautogui.typewrite('grep -i \'exception\' /data/logs/WS_SPPS_J_0.log.' + now_date())
+        pyautogui.hotkey('enter')
+        time.sleep(20)
+        pyautogui.hotkey('ctrl', 'c')
+        time.sleep(2)
+        pyautogui.typewrite('grep -i \'exception\' /data/logs/WS_SPPS_J_1.log.' + now_date())
+        pyautogui.hotkey('enter')
+        time.sleep(20)
+        pyautogui.hotkey('ctrl', 'c')
+        time.sleep(1)
         pyautogui.typewrite('date; mpstat | tail -1 | awk \'{print 100-$NF}\'; free -m; df -h')
         pyautogui.hotkey('enter')
         time.sleep(1)
@@ -197,6 +203,16 @@ while cnt < 1:
                 pyautogui.typewrite('tail -f /data/logs/SPPS_J_1.log.' + now_date())
                 pyautogui.hotkey('enter')
                 time.sleep(10)
+                pyautogui.hotkey('ctrl', 'c')
+                time.sleep(1)
+                pyautogui.typewrite('grep -i \'exception\' /data/logs/SPPS_J_0.log.' + now_date())
+                pyautogui.hotkey('enter')
+                time.sleep(20)
+                pyautogui.hotkey('ctrl', 'c')
+                time.sleep(2)
+                pyautogui.typewrite('grep -i \'exception\' /data/logs/SPPS_J_1.log.' + now_date())
+                pyautogui.hotkey('enter')
+                time.sleep(20)
                 pyautogui.hotkey('ctrl', 'c')
                 time.sleep(1)
                 pyautogui.typewrite('date; mpstat | tail -1 | awk \'{print 100-$NF}\'; free -m; df -h')
@@ -291,6 +307,11 @@ while cnt < 1:
                                         time.sleep(10)
                                         pyautogui.hotkey('ctrl', 'c')
                                         time.sleep(1)
+                                        pyautogui.typewrite('cat /data/logs/spcs/spcs.' + now_date() + '.log | grep -i \'exception\'')
+                                        pyautogui.hotkey('enter')
+                                        time.sleep(20)
+                                        pyautogui.hotkey('ctrl', 'c')
+                                        time.sleep(1)
                                         pyautogui.typewrite('date; mpstat | tail -1 | awk \'{print 100-$NF}\'; free -m; df -h')
                                         pyautogui.hotkey('enter')
                                         time.sleep(1)
@@ -317,6 +338,11 @@ while cnt < 1:
                                                 time.sleep(10)
                                                 pyautogui.hotkey('ctrl', 'c')
                                                 time.sleep(1)
+                                                pyautogui.typewrite('cat /data/logs/push/push.' + now_date() + '.log | grep -i \'exception\'')
+                                                pyautogui.hotkey('enter')
+                                                time.sleep(20)
+                                                pyautogui.hotkey('ctrl', 'c')
+                                                time.sleep(1)
                                                 pyautogui.typewrite('date; mpstat | tail -1 | awk \'{print 100-$NF}\'; free -m; df -h')
                                                 pyautogui.hotkey('enter')
                                                 time.sleep(1)
@@ -341,6 +367,11 @@ while cnt < 1:
                                                         pyautogui.typewrite('tail -f /data/logs/spls/spls.log')
                                                         pyautogui.hotkey('enter')
                                                         time.sleep(10)
+                                                        pyautogui.hotkey('ctrl', 'c')
+                                                        time.sleep(1)
+                                                        pyautogui.typewrite('cat /data/logs/spls/spls.log | grep -i \'exception\'')
+                                                        pyautogui.hotkey('enter')
+                                                        time.sleep(20)
                                                         pyautogui.hotkey('ctrl', 'c')
                                                         time.sleep(1)
                                                         pyautogui.typewrite('date; mpstat | tail -1 | awk \'{print 100-$NF}\'; free -m; df -h')
@@ -400,84 +431,4 @@ while cnt < 1:
                                                                 break
                                                             if cnt == 21:
                                                                 break
-f1.close()
-f2.close()
-f3.close()
-f4.close()
-
-load_wb = openpyxl.Workbook()
-def create_excel():
-    load_ws = load_wb.active
-    load_wb.create_sheet(index=1, title='total_sheet')
-
-if __name__ == "__main__":
-    create_excel()
-
-def create_date():
-    cell = ['G1', 'F1', 'E1', 'D1', 'C1', 'B1', 'A1']
-    for i in range(-7, 0):
-        day_delta = datetime.timedelta(days=1)
-        start_date = datetime.date.today()
-        a_week_ago = start_date + i * datetime.timedelta(1)
-        result = a_week_ago.strftime('%Y-%m-%d')
-        load_ws2 = load_wb['total_sheet']
-        load_ws2[cell.pop()] = result
-
-        # 금 ~ 목 API 이력관리 엑셀 파일 다운
-        driver.get('http://172.16.1.1/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
-
-# 상용 cms 로그인
-while True:
-    try:
-        driver = webdriver.Chrome(r'C:\Users\webiznet\Desktop\기타\chromedriver.exe')
-        driver.get('http://172.16.1.1/spa/account/login.do')
-        driver.find_element_by_name('j_username').send_keys('admin')
-        driver.find_element_by_name('j_password').send_keys('passwd')
-        driver.find_element_by_xpath("""//*[@id="loginForm"]/fieldset/p/input""").click()
-
-        for k in range(-7, 0):  # 금 ~ 목 시트 생성 후, 일별 데이터 저장
-            day_delta = datetime.timedelta(days=1)
-            start_date = datetime.date.today()
-            a_week_ago = start_date + k * datetime.timedelta(1)
-            result = a_week_ago.strftime('%Y%m%d')
-            load_wb.create_sheet(index=1, title=result)
-            load_ws = load_wb[result]
-            driver.get('http://172.16.1.1/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
-            time.sleep(1)
-            for a_tag in soup.findAll('td', class_='ac'):
-                result1 = (a_tag.get_text().strip().replace('\n', '').replace('\t', ''))
-                load_ws = load_wb[result]
-                load_ws.append([result1])
-
-        cell1 = ['G2', 'F2', 'E2', 'D2', 'C2', 'B2', 'A2']
-        for j in range(-7, 0):  # 최댓값 비교
-            day_delta = datetime.timedelta(days=1)
-            start_date = datetime.date.today()
-            a_week_ago = start_date + j * datetime.timedelta(1)
-            result = a_week_ago.strftime('%Y%m%d')
-            load_ws = load_wb[result]
-            if load_ws['A6'].value < load_ws['A9'].value:
-                temp3 = load_ws['A9'].value
-            else:
-                temp3 = load_ws['A6'].value
-            for q in range(9, 76, 3):
-                n3 = load_ws['A' + str(q)].value
-                if temp3 < n3:
-                    temp3 = n3
-                else:
-                    pass
-            load_ws2 = load_wb['total_sheet']
-            load_ws2[cell1.pop()] = temp3
-            print(result, temp3)
-
-    except NoSuchElementException as e:
-        print('retry', e)
-        continue
-        
-    break
-
-create_date()
-load_wb.remove(load_wb['Sheet'])
-load_wb.save('crawlling.xlsx')
+f1.close(); f2.close(); f3.close(); f4.close()
