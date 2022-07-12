6a09643b09af7aa37e76cb40fece827826bfedd8
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2022-07-13 01:44:52 +0900

    문서 자동화
    문서 자동화

--- wcs_docx_auto.py
+++ wcs_docx_auto.py
@@ -12,13 +12,12 @@ def now_date():
     return result
 
 try:
-    #document = Document('C:/Users/LG/Desktop/WEB3.0_플랫폼_운영보고서_20200909.docx')
-    document = Document('C:/Users/webiznet/Desktop/Olleh_TV_홈포털_플랫폼_운영보고서_' + now_date() + '.docx')
+    document = Document('C:/Users/ljs91/Desktop/Olleh_TV_홈포털_플랫폼_운영보고서_' + now_date() + '.docx')
 
-    file_list = ['C:/Users/webiznet/Desktop/QAT_sample/수요일_정기점검_모니터링샘플데이터0819/capture/이미지 048.png',
-                 'C:/Users/webiznet/Desktop/QAT_sample/수요일_정기점검_모니터링샘플데이터0819/capture/이미지 049.png']
-    file_list1 = ['C:/Users/webiznet/Desktop/QAT_sample/수요일_정기점검_모니터링샘플데이터0819/capture/ocr대상/이미지 046.png',
-                  'C:/Users/webiznet/Desktop/QAT_sample/수요일_정기점검_모니터링샘플데이터0819/capture/ocr대상/이미지 047.png']
+    file_list = ['C:/Users/ljs91/Desktop/WCS_정기점검_샘플데이터/capture/이미지 045.png',
+                 'C:/Users/ljs91/Desktop/WCS_정기점검_샘플데이터/capture/이미지 046.png']
+    file_list1 = ['C:/Users/ljs91/Desktop/WCS_정기점검_샘플데이터/capture/ocr대상/이미지 043.png',
+                  'C:/Users/ljs91/Desktop/WCS_정기점검_샘플데이터/capture/ocr대상/이미지 044.png']
 
     tables = document.tables
     num = 6
@@ -26,7 +25,7 @@ try:
         p = tables[num].rows[0].cells[0].add_paragraph()
         r = p.add_run()
         r.add_picture(i, width=Inches(6.7))
-        num +=1
+        num += 1
         if num == 8:
             for k in file_list1:
                 p = tables[num].rows[0].cells[0].add_paragraph()
@@ -34,7 +33,7 @@ try:
                 r.add_picture(k, width=Inches(6.7))
                 num += 1
                 if num == 10: break
-    document.save('C:/Users/webiznet/Desktop/Olleh_TV_홈포털_플랫폼_운영보고서_' + now_date() + '.docx')
+    document.save('C:/Users/ljs91/Desktop/Olleh_TV_홈포털_플랫폼_운영보고서_' + now_date() + '.docx')
 
 except NameError as e:
     print('Error', e)
