4a8bfdaf0d3e5a968d4087cb6584acfdd42528cc
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2022-07-13 01:40:08 +0900

    putty 자동 연결
    putty 자동 연결

--- ssh_auto.py
+++ ssh_auto.py
@@ -1,8 +1,5 @@
 import paramiko
 
-# ssh 프로그램을 이용한 서버 접속이 가능한 경우, 1번 파일에 서버 리스트, 2번 파일에 명령어를 작성하여 실행
-# kth tb 환경이나, 개인 vm에 활용 가능
-
 def ssh_auto():
     f1 = open('ssh_server_list.txt', 'r')
     f2 = open('ssh_command.txt', 'r')
@@ -30,7 +27,7 @@ def ssh_auto():
                 f2 = open('ssh_command.txt', 'r')
 
         except TimeoutError as e:  # 서버 연결 안될 경우 해당 서버는 패스
-            print('**' + line1 + '**' ' connection time out, pass !!', e)
+            print('**' + line1 + '**' ' connection time out, pass ', e)
             pass; continue
 
         except paramiko.ssh_exception.NoValidConnectionsError as e:
