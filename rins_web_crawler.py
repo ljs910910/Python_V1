64c8049eec6920aa4e5c2e318ef4015f137a3ab9
ljs910910 <47237317+ljs910910@users.noreply.github.com>
2022-07-13 01:37:47 +0900

    RINS 웹 크롤러
    RINS 웹 크롤러

--- rins_web_crawler.py
+++ rins_web_crawler.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-import datetime
-import time
+import datetime, time
 import openpyxl
 
 load_wb = openpyxl.Workbook()
@@ -22,16 +21,17 @@ def create_date():
         load_ws2[cell.pop()] = result
 
         # 금 ~ 목 API 이력관리 엑셀 파일 다운
-        driver.get('http://172.16.1.1/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' \
-                   + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
+        driver.get(
+            'http://172.16.42.50/spa/admin/services/apiLog/excelDownload.do?searchResult=&apiGroupCd=&searchStDt=' \
+            + result + '&searchStHH=00&searchStMM=00&searchEdDt=' + result + '&searchEdHH=23&searchEdMM=59&logContent=')
 
 while True:
     try:
         # 상용 cms 로그인
-        driver = webdriver.Chrome(r'C:\Users\user\Desktop\기타\chromedriver.exe')
-        driver.get('http://172.16.1.1/spa/account/login.do')
-        driver.find_element_by_name('j_username').send_keys('admin')
-        driver.find_element_by_name('j_password').send_keys('passwd')
+        driver = webdriver.Chrome(r'D:\chromedriver\chromedriver.exe')
+        driver.get('http://172.16.42.50/spa/account/login.do')
+        driver.find_element_by_name('j_username').send_keys('rins_admin')
+        driver.find_element_by_name('j_password').send_keys('ipaleldj1!')
         driver.find_element_by_xpath("""//*[@id="loginForm"]/fieldset/p/input""").click()
 
         for k in range(-7, 0):  # 금 ~ 목 시트 생성 후, 일별 데이터 저장
@@ -40,7 +40,7 @@ while True:
             result = a_week_ago.strftime('%Y%m%d')
             load_wb.create_sheet(index=1, title=result)
             load_ws = load_wb[result]
-            driver.get('http://172.16.1.1/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
+            driver.get('http://172.16.42.50/spa/admin/mng/pushmng/timetraffic.ajax?searchDt=' + str(result))
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
             time.sleep(1)
